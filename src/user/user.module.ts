ce729762427d1b762a9f65bd8678a0f7e3f7b8e4
emvalencaf <edsonmvf@gmail.com>
2023-04-28 00:48:06 -0300

    refactored: changed interface to entities and integrate typeorm and postgres db

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,14 +1,20 @@
 // decorators
 import { Module } from '@nestjs/common';
 
+// modules
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 // controllers
 import { UserController } from './user.controller';
 
 // services
 import { UserService } from './user.service';
 
+// entities
+import { UserEntity } from './entity/user.entity';
+
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UserController],
   providers: [UserService],
   exports: [],
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,18 +1,22 @@
 // decorators
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
 // dtos
 import { CreateUserDTO } from './dtos/create-user.dto';
 
-// interfaces
-import { UserI } from './interfaces/user.interface';
+// entities
+import { UserEntity } from './entity/user.entity';
 
 // utils
 import { genSalt, hash } from 'bcrypt';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
-  private users: UserI[] = [];
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
 
   // create a new user
   async create({
@@ -21,29 +25,26 @@ export class UserService {
     phone,
     email,
     cpf,
-  }: CreateUserDTO): Promise<UserI> {
+  }: CreateUserDTO): Promise<UserEntity> {
     // turn password into an hash
     const salt = await genSalt();
     const passwordHash = await hash(password, salt);
 
     // create user data
-    const user = {
+    const data = {
       name,
       password: passwordHash,
       cpf,
       phone,
       email,
-      id: this.users.length + 1,
     };
 
-    // insert new user in the memory
-    this.users.push(user);
-
-    return user;
+    // create and return an new user
+    return await this.userRepository.save(data);
   }
 
   // get all users
-  async getAllUsers(): Promise<UserI[]> {
-    return this.users;
+  async getAllUsers(): Promise<UserEntity[]> {
+    return this.userRepository.find();
   }
 }
